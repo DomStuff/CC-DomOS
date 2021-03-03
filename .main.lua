b5f5edc8e100503919bc5b24c378f795ec05e186
DomStuff <43822029+DomStuff@users.noreply.github.com>
2021-03-03 14:42:11 +0000

    Create .main.lua

---
+++ .main.lua
@@ -0,0 +1,22 @@
+term.clear()
+
+local w, h = term.getSize()
+
+local selection = 1
+
+local function drawButton(x,y, num, name)
+    term.setCursorPos(x,y)
+    if selection == num then
+        term.write(">" + name)
+    else
+        term.write(name)
+    end
+end 
+local function drawMenu()
+    term.clear()
+    term.setCursorPos(1, 1)
+    
+    drawButton(2,3,1,"Test")
+end
+
+drawMenu()
