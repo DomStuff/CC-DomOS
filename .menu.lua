35bc69c6bacf32a4e42f32cec487534a795aa672
DomStuff <43822029+DomStuff@users.noreply.github.com>
2021-03-04 23:04:38 +0000

    Update .menu.lua

--- .menu.lua
+++ .menu.lua
@@ -5,7 +5,7 @@ local w, h = term.getSize()
 local selection = 1
 
 local function drawMenu()
-    local mp, mb, mx, my = os.pullEvent("mouse_clicked")
+    local mp, mb, mx, my = os.pullEvent("mouse_click")
     term.clear()
     term.setCursorPos(1, 1)
     print("Mouse X: " .. mx)
