dafa494b4c6ce278233c105d6616b70feacb2819
DomStuff <43822029+DomStuff@users.noreply.github.com>
2021-03-04 22:43:45 +0000

    Update .update.lua

--- .update.lua
+++ .update.lua
@@ -1,25 +1,2 @@
 term.clear()
-
-term.setCursorPos(1,1)
-term.write("Updating")
-
-term.setCursorPos(1,3)
-term.write("---")
-term.setCursorPos(1,3)
-
-function install(name)
-    local fileName = name
-	local getResponse = http.get("https://raw.githubusercontent.com/DomStuff/CC-DomOS/main/" .. fileName)
-	local file = fs.open(fileName, "w")
-	file.write(getResponse.readAll())
-	file.close()
-	getResponse.close()
-	term.write("#")
-end
-
-install("startup.lua")
-install(".menu.lua")
-install(".update.lua")
-
-term.setCursorPos(1,5)
-shell.run("reboot")
+shell.run("pastebin", "run", "4kBD3e5n")
