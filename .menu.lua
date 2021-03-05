19724f608d40a06057efd77626bff33e32d0731b
DomStuff <43822029+DomStuff@users.noreply.github.com>
2021-03-05 09:54:34 +0000

    Update .menu.lua

--- .menu.lua
+++ .menu.lua
@@ -8,13 +8,13 @@ local function drawMenu()
     term.setBackgroundColour(colours.white)
     term.setTextColour(colours.black)
     term.clear()
-end
-
-while true do
-    drawMenu()
     
     local mp, mb, mx, my = os.pullEvent("mouse_click")
     
     term.setCursorPos(mx-1, my)
     term.write("OwO")
+end
+
+while true do
+    drawMenu()
 end
