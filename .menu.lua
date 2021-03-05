9d01e373e22ea2e5464f50c3bab1c852b29f2374
DomStuff <43822029+DomStuff@users.noreply.github.com>
2021-03-05 09:05:25 +0000

    Update .menu.lua

--- .menu.lua
+++ .menu.lua
@@ -5,13 +5,12 @@ local w, h = term.getSize()
 local selection = 1
 
 local function drawMenu()
-    local mp, mb, mx, my = os.pullEvent("mouse_click")
     term.setBackgroundColour(colours.white)
     term.clear()
-    
-    
 end
 
 while true do
     drawMenu()
+    
+    local mp, mb, mx, my = os.pullEvent("mouse_click")
 end
