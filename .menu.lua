cc2e6b08c41964739830cb90c68fcdcb7de716c4
DomStuff <43822029+DomStuff@users.noreply.github.com>
2021-03-05 08:33:01 +0000

    Update .menu.lua

--- .menu.lua
+++ .menu.lua
@@ -8,16 +8,16 @@ local function drawMenu()
     local mp, mb, mx, my = os.pullEvent("mouse_click")
     term.clear()
     
-    term.setCursorPos(w/2-7, 1)
+    term.setCursorPos(w/2-6, 1)
     term.write("Mouse X|" .. mx)
     
-    term.setCursorPos(w/2-7, 2)
+    term.setCursorPos(w/2-6, 2)
     term.write("Mouse Y|" .. my)
     
-    term.setCursorPos(w/2-6, 3)
+    term.setCursorPos(w/2-5, 3)
     term.write("Button|" .. mb)
     
-    term.setCursorPos(w/2-7, 4)
+    term.setCursorPos(w/2-6, 4)
     term.write("Pressed|" .. mp)
 end
 
